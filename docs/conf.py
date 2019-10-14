8f602527bd45a611d3a3f4d4493fa00c3a830ad4
Nina Jensen <jnik@aarhus.dk>
2019-10-14 16:16:29 +0200

    Update conf.py with autodoc import mocking

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,8 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+# List of modules to mock imports for
+autodoc_mock_imports = ["pandas"]
 
 # -- Options for HTML output -------------------------------------------------
 
