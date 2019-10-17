261a16e7764365a76456bac49572a13f94d75304
Nina Jensen <jnik@aarhus.dk>
2019-10-17 14:46:38 +0200

    Add data.py and FileInfo data class.

---
+++ digital_archive/data.py
@@ -0,0 +1,25 @@
+"""Implements data classes used throughout the package.
+
+"""
+
+# -----------------------------------------------------------------------------
+# Imports
+# -----------------------------------------------------------------------------
+from dataclasses import dataclass, asdict
+
+# -----------------------------------------------------------------------------
+# Classes
+# -----------------------------------------------------------------------------
+@dataclass
+class FileInfo:
+    """Class for keeping track of file information"""
+
+    name: str = ""
+    ext: str = ""
+    is_empty_sub: bool = False
+    path: str = ""
+    mime_type: str = ""
+    guessed_ext: str = ""
+
+    def as_dict(self):
+        return asdict(self)
