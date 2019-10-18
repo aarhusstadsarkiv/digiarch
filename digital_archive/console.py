e47b22c16873b8bf0b90fd295efa50901816d4f5
Nina Jensen <jnik@aarhus.dk>
2019-10-18 15:06:06 +0200

    Update console with message functions and type hints.

--- digital_archive/console.py
+++ digital_archive/console.py
@@ -16,8 +16,18 @@ from .identify import reports
 # -----------------------------------------------------------------------------
 
 
+def click_ok(message: str) -> None:
+    click.secho("🗸 ", fg="green", nl=False)
+    click.echo(message)
+
+
+def click_warn(message: str) -> None:
+    click.secho("⚠️ ", fg="red", nl=False)
+    click.echo(message)
+
+
 @click.group()
-def cli():
+def cli() -> None:
     """Command Line Tool for handling Aarhus Digital Archive handins."""
     pass
 
@@ -26,10 +36,17 @@ def cli():
 @click.option(
     "--path", type=click.Path(exists=True, file_okay=False, resolve_path=True)
 )
-def report(path):
+def report(path: str) -> None:
     """This command invokes `report_results` on files and
     subdirectories found in the given `--path`."""
     # TODO: --path should be optional, default to directory where
     # the CLI is called.
-    file_exts, empty_dirs = path_utils.explore_dir(path)
-    reports.report_results(file_exts, empty_dirs, path)
+    # TODO: Check if path is empty, exit gracefully if so.
+    click.secho("Collecting file information...", bold=True)
+    dir_info = path_utils.explore_dir(path)
+    click.secho("Done!", bold=True, fg="green")
+    report_file, empty_subs = reports.report_results(dir_info, path)
+    click_ok(f"Wrote file extension report to {report_file}")
+    if empty_subs:
+        click_warn("There are empty subdirectories!")
+        click_warn(f"Consult {empty_subs} for more information.")
