24a90824e0fa2409197b5ca29b38c0d91b7a69c9
Nina Jensen <jnik@aarhus.dk>
2019-10-21 12:50:04 +0200

    Add json encoder class to data.py + to_json function in FileInfo

--- digital_archive/data.py
+++ digital_archive/data.py
@@ -5,12 +5,14 @@
 # -----------------------------------------------------------------------------
 # Imports
 # -----------------------------------------------------------------------------
-from dataclasses import dataclass, asdict
+import dataclasses
+import json
+from typing import Optional
 
 # -----------------------------------------------------------------------------
 # Classes
 # -----------------------------------------------------------------------------
-@dataclass
+@dataclasses.dataclass
 class FileInfo:
     """Class for keeping track of file information"""
 
@@ -22,4 +24,15 @@ class FileInfo:
     guessed_ext: str = ""
 
     def to_dict(self) -> dict:
-        return asdict(self)
+        return dataclasses.asdict(self)
+
+    def to_json(self) -> str:
+        return json.dumps(self, cls=FileInfoEncoder)
+
+
+class FileInfoEncoder(json.JSONEncoder):
+    def default(self, file_info: FileInfo) -> Optional(dict):
+        # Since type checking is not enforced at runtime
+        if isinstance(file_info, FileInfo):
+            return file_info.to_dict()
+        return super().default(file_info)
