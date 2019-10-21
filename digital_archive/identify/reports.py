1b15b246f62f7d91bde0654a89122e4922ed2542
Nina Jensen <jnik@aarhus.dk>
2019-10-21 16:05:40 +0200

    Add click_utils, update reports to not return and call click echo instead

--- digital_archive/identify/reports.py
+++ digital_archive/identify/reports.py
@@ -8,14 +8,15 @@
 import os
 import pandas as pd
 from digital_archive.data import FileInfo
-from typing import List, Tuple
+from digital_archive.utils import click_utils
+from typing import List
 
 # -----------------------------------------------------------------------------
 # Function Definitions
 # -----------------------------------------------------------------------------
 
 
-def report_results(info: List[FileInfo], save_path: str) -> Tuple[str, str]:
+def report_results(info: List[FileInfo], save_path: str) -> None:
     """Generates reports of explore_dir() results.
 
     Parameters
@@ -45,12 +46,17 @@ def report_results(info: List[FileInfo], save_path: str) -> Tuple[str, str]:
     files = [f.to_dict() for f in info if f.is_empty_sub is False]
     empty_subs = [f.path for f in info if f.is_empty_sub is True]
 
-    # Generate reports
-    report_file = os.path.join(save_path, "file_exts.csv")
-    files_df = pd.DataFrame(data=files)
-    # Count extensions
-    file_exts_count = files_df.groupby("ext").size().rename("count").to_frame()
-    file_exts_count.to_csv(report_file, header=True)
+    # We might get an empty directory
+    if files:
+        # Generate reports
+        report_file = os.path.join(save_path, "file_exts.csv")
+        files_df = pd.DataFrame(data=files)
+        # Count extensions
+        file_exts_count = (
+            files_df.groupby("ext").size().rename("count").to_frame()
+        )
+        file_exts_count.to_csv(report_file, header=True)
+        click_utils.click_ok(f"Wrote file extension report to {report_file}")
 
     # Generate separate report if there are empty subdirectories
     if empty_subs:
@@ -58,5 +64,5 @@ def report_results(info: List[FileInfo], save_path: str) -> Tuple[str, str]:
         with open(empty_subs_file, "w+") as f:
             for sub in empty_subs:
                 f.write(sub + "\n")
-
-    return report_file, empty_subs_file
+        click_utils.click_warn("There are empty subdirectories!")
+        click_utils.click_warn(f"Consult {empty_subs} for more information.")
--- tests/identify/test_reports.py
+++ tests/identify/test_reports.py
@@ -1,35 +1,35 @@
 from typing import List
 from digital_archive.identify.reports import report_results
+from digital_archive.data import FileInfo
 
 
 class TestReportResults:
     """Class for testing the `report_results` function."""
 
-    no_files: List = []
-    no_folders: List = []
-    files: List[List[str]] = [
-        [".txt", "/root/"],
-        [".png", "/root/"],
-        [".pdf", "/root/"],
-    ]
-    folders: List[str] = ["/path/to/empty/folder1", "/path/to/empty/folder2"]
+    no_dir_info: List = []
+    file_info = FileInfo(
+        name="test.txt", ext=".txt", is_empty_sub=False, path="/root/"
+    )
+    empty_sub_info = FileInfo(is_empty_sub=True, path="/root/")
+    dir_info_file = [file_info]
+    dir_info_file_empty_sub = [file_info, empty_sub_info]
 
     def test_no_files_no_folders(self, tmpdir):
-        """`report_results` is invoked with empty files and folders lists.
+        """`report_results` is invoked with an empty list of FileInfo objects.
         No files expected."""
-        report_results(self.no_files, self.no_folders, tmpdir)
+        report_results(self.no_dir_info, tmpdir)
         assert len(tmpdir.listdir()) == 0
 
     def test_files_no_folders(self, tmpdir):
-        """`report_results` is invoked with a populated file list
-        and an empty folders list.
+        """`report_results` is invoked with a populated list of
+        FileInfo objects. None of them are empty subfolders.
         1 file expected to be written."""
-        report_results(self.files, self.no_folders, tmpdir)
+        report_results(self.dir_info_file, tmpdir)
         assert len(tmpdir.listdir()) == 1
 
     def test_files_and_folders(self, tmpdir):
-        """`report_results` is invoked with a populated file list
-        and a populated folders list.
+        """`report_results` is invoked with a populated list of
+        FileInfo objects. One is a file, one is an empty subfolder.
         2 files expected to be written."""
-        report_results(self.files, self.folders, tmpdir)
+        report_results(self.dir_info_file_empty_sub, tmpdir)
         assert len(tmpdir.listdir()) == 2
