340132786421ff0db859c839f78585fd1a477ca1
Nina Jensen <jnik@aarhus.dk>
2019-10-21 16:08:22 +0200

    Comment out identify_files as not in use atm

--- digital_archive/identify/identify_files.py
+++ digital_archive/identify/identify_files.py
@@ -12,25 +12,25 @@
 # -----------------------------------------------------------------------------
 
 
-def this_is_my_function(input: None) -> None:
-    """Function level documentation.
-    Delete non-applicable sections.
+# def this_is_my_function(input: None) -> None:
+#     """Function level documentation.
+#     Delete non-applicable sections.
 
-    Parameters
-    ----------
-    input : type
-        description
+#     Parameters
+#     ----------
+#     input : type
+#         description
 
-    Returns
-    -------
-    return : type
-        description
-    type (anonymous types are allowed in return)
-        description
-    Raises
-    ------
-    BadException
-        description
+#     Returns
+#     -------
+#     return : type
+#         description
+#     type (anonymous types are allowed in return)
+#         description
+#     Raises
+#     ------
+#     BadException
+#         description
 
-    """
-    pass
+#     """
+#     pass
