534489cb9e750d735542f89e937f8593764f7474
Nina Jensen <jnik@aarhus.dk>
2019-11-13 11:27:05 +0100

    Fix JSON unicode bug (issue 94) (#95)
    * Update readme to reflect name change (#89)
    
    * Update README.md
    
    * Update README.md
    
    * Add sorting by ext count to file_exts report (#91)
    
    * Iss93 json unicode bug (#94)
    
    * Update data with JSONEncoder and dump_file function
    
    * Update path utils with dump_file call
    
    * Fix tests as they were suddenly failing miserably!
    
    * Remove tqdm from data imports
    
    * Disable pylint messages in DataJSONEncoder (#96)
    
    * Update data with JSONEncoder and dump_file function
    
    * Update path utils with dump_file call
    
    * Fix tests as they were suddenly failing miserably!
    
    * Remove tqdm from data imports
    
    * Add disable comment for pylint
    It does not like subclassing of JSONEncoder
    
    * Fix white space

--- digiarch/data.py
+++ digiarch/data.py
@@ -9,12 +9,14 @@ Digital Archive.
 import dataclasses
 import dacite
 import json
-import tqdm
 from typing import Any, List
 
 # -----------------------------------------------------------------------------
 # Classes
 # -----------------------------------------------------------------------------
+
+# File Info
+# --------------------
 
 
 @dataclasses.dataclass
@@ -32,27 +34,61 @@ class FileInfo:
         """Avoid having to import dataclasses all the time."""
         return dataclasses.asdict(self)
 
-    def to_json(self) -> str:
-        """Return json dump using
-        :class:`~digiarch.data.DataclassEncoder`"""
-        return json.dumps(self, default=encode_dataclass)
-
     @staticmethod
     def from_dict(data: dict) -> Any:
         return dacite.from_dict(data_class=FileInfo, data=data)
 
 
+# Utility
+# --------------------
+
+
+class DataJSONEncoder(json.JSONEncoder):
+    """DataJSONEncoder subclasses JSONEncoder in order to handle
+    encoding of dataclasses."""
+
+    def default(self, obj: object) -> Any:  # pylint: disable=E0202,W0221
+        """Overrides the JSONEncoder default.
+
+        Parameters
+        ----------
+        obj : object
+            Object to encode.
+        Returns
+        -------
+        dataclasses.asdict(obj) : dict
+            If the object given is a data class, return it as a dict.
+        super().default(obj) : Any
+            If the object is not a data class, use JSONEncoder's default and
+            let it handle any exception that might occur.
+        """
+        if dataclasses.is_dataclass(obj):
+            return dataclasses.asdict(obj)
+        return super().default(obj)
+
+
 # -----------------------------------------------------------------------------
 # Function Definitions
 # -----------------------------------------------------------------------------
-def encode_dataclass(data_cls: object) -> dict:
-    try:
-        return dataclasses.asdict(data_cls)
-    except TypeError:
-        type_name = type(data_cls)
-        raise TypeError(
-            f"Object of type {type_name} is not serializable with this default"
-        )
+
+
+def dump_file(data: object, file: str) -> None:
+    """Dumps JSON files given data and a file path
+    using :class:`~digiarch.data.DataJSONEncoder` as encoder.
+    Output uses indent = 4 to get pretty and readable files.
+    `ensure_ascii` is set to `False` so we can get our beautiful Danish
+    letters in the output.
+
+    Parameters
+    ----------
+    data : object
+        The data to dump to the JSON file.
+    dump_file: str
+        Path to the file in which to dump JSON data.
+    """
+
+    with open(file, "w+") as f:
+        json.dump(data, f, indent=4, cls=DataJSONEncoder, ensure_ascii=False)
 
 
 def load_json_list(data_file: str) -> List[dict]:
@@ -63,11 +99,8 @@ def load_json_list(data_file: str) -> List[dict]:
 
 def get_fileinfo_list(data_file: str) -> List[FileInfo]:
     # Read file info from data file
-    data: List[dict] = tqdm.tqdm(
-        load_json_list(data_file), desc="Reading file information"
-    )
+    data: List[dict] = load_json_list(data_file)
+
     # Load file info into list
-    info: List[FileInfo] = tqdm.tqdm(
-        [FileInfo.from_dict(d) for d in data], desc="Loading file information"
-    )
+    info: List[FileInfo] = [FileInfo.from_dict(d) for d in data]
     return info
