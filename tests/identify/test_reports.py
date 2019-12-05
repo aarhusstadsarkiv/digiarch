f3ee4f8a20cf23b86506a81dd52454cb796c18ed
Nina Jensen <jnik@aarhus.dk>
2019-12-05 15:23:11 +0100

    Update tests to reflect changes

--- tests/identify/test_reports.py
+++ tests/identify/test_reports.py
@@ -11,13 +11,9 @@ def write_test_file(temp_dir, data_file, dir_info):
 class TestReportResults:
     """Class for testing the `report_results` function."""
 
-    file_info = FileInfo(
-        name="test.txt", ext=".txt", is_empty_sub=False, path="/root/"
-    )
-    empty_sub_info = FileInfo(is_empty_sub=True, path="/root/")
+    file_info = FileInfo(name="test.txt", ext=".txt", path="/root/")
     no_dir_info: List = []
     dir_info_file = [file_info]
-    dir_info_file_empty_sub = [file_info, empty_sub_info]
 
     def test_no_files_no_folders(self, temp_dir, data_file):
         """`report_results` is invoked with an empty list of FileInfo objects.
@@ -45,15 +41,15 @@ class TestReportResults:
         ]
         assert len(files_in_temp) == 1
 
-    def test_files_and_folders(self, temp_dir, data_file):
-        """`report_results` is invoked with a populated list of
-        FileInfo objects. One is a file, one is an empty subfolder.
-        2 files expected to be written."""
-        write_test_file(temp_dir, data_file, self.dir_info_file_empty_sub)
-        report_results(data_file, temp_dir)
-        files_in_temp = [
-            file
-            for file in os.listdir(temp_dir)
-            if os.path.isfile(os.path.join(temp_dir, file))
-        ]
-        assert len(files_in_temp) == 2
+    # def test_files_and_folders(self, temp_dir, data_file):
+    #     """`report_results` is invoked with a populated list of
+    #     FileInfo objects. One is a file, one is an empty subfolder.
+    #     2 files expected to be written."""
+    #     write_test_file(temp_dir, data_file, self.dir_info_file_empty_sub)
+    #     report_results(data_file, temp_dir)
+    #     files_in_temp = [
+    #         file
+    #         for file in os.listdir(temp_dir)
+    #         if os.path.isfile(os.path.join(temp_dir, file))
+    #     ]
+    #     assert len(files_in_temp) == 2
