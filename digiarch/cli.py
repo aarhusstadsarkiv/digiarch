c8490f79712c1aee677e4e6ed708d7d52cf3f282
Nina Jensen <jnik@aarhus.dk>
2019-12-06 11:30:07 +0100

    Update cli with new split re duplicates/checksums

--- digiarch/cli.py
+++ digiarch/cli.py
@@ -9,7 +9,7 @@ The CLI implements several commands with suboptions.
 # -----------------------------------------------------------------------------
 import click
 import os
-from pathlib import Path
+from digiarch.data import get_fileinfo_list, dump_file
 from digiarch.utils import path_utils, group_files
 from digiarch.identify import checksums
 from digiarch.identify import reports
@@ -72,7 +72,9 @@ def group(path_info: dict) -> None:
 @click.pass_obj
 def checksum(path_info: dict) -> None:
     """Generate file checksums using BLAKE2."""
-    checksums.generate_checksums(Path(path_info["data_file"]))
+    files = get_fileinfo_list(path_info["data_file"])
+    updated_files = checksums.generate_checksums(files)
+    dump_file(updated_files, path_info["data_file"])
     click.secho("Done!", bold=True, fg="green")
 
 
@@ -80,8 +82,5 @@ def checksum(path_info: dict) -> None:
 @click.pass_obj
 def dups(path_info: dict) -> None:
     """Check for file duplicates."""
-    collisions: int = checksums.check_collisions(
-        path_info["data_file"], path_info["main_dir"]
-    )
-    click.secho(f"Found {collisions} collisions.")
+    checksums.check_duplicates(path_info["data_file"], path_info["main_dir"])
     click.secho("Done!", bold=True, fg="green")
