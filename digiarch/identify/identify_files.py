617e89ff530469bab30201280ec5ed2659e437f3
Nina Jensen <jnik@aarhus.dk>
2019-12-12 14:40:24 +0100

    Update identify files with call to siegfried

--- digiarch/identify/identify_files.py
+++ digiarch/identify/identify_files.py
@@ -5,32 +5,48 @@
 # -----------------------------------------------------------------------------
 # Imports
 # -----------------------------------------------------------------------------
-# import magic
+import subprocess
+from subprocess import CalledProcessError
+from digiarch.data import FileInfo
+import yaml
 
 # -----------------------------------------------------------------------------
 # Function Definitions
 # -----------------------------------------------------------------------------
 
 
-# def this_is_my_function(input: None) -> None:
-#     """Function level documentation.
-#     Delete non-applicable sections.
+def sf_id(file: FileInfo) -> FileInfo:
+    """Function level documentation.
+    Delete non-applicable sections.
 
-#     Parameters
-#     ----------
-#     input : type
-#         description
+    Parameters
+    ----------
+    input : type
+        description
 
-#     Returns
-#     -------
-#     return : type
-#         description
-#     type (anonymous types are allowed in return)
-#         description
-#     Raises
-#     ------
-#     BadException
-#         description
+    Returns
+    -------
+    return : type
+        description
+    type (anonymous types are allowed in return)
+        description
+    Raises
+    ------
+    BadException
+        description
 
-#     """
-#     pass
+    """
+    cmd = subprocess.run(f"sf {file.path}", shell=True, capture_output=True)
+    docs = yaml.safe_load_all(cmd.stdout)
+    for doc in docs:
+        if doc.get("matches"):
+            return doc.get("matches")[0]
+
+
+file = FileInfo(
+    name="test_sheet",
+    ext="",
+    path="/home/jnik/Documents/test_data/test_sheet",
+    checksum="",
+)
+sf_id(file)
