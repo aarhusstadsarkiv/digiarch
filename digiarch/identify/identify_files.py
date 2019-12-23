fe4af4ebbc569f18cb45aec23064275edd445b91
Nina Jensen <jnik@aarhus.dk>
2019-12-23 12:54:13 +0100

    Comment out identify_files as it gives prospector errors Will fix in different branch

--- digiarch/identify/identify_files.py
+++ digiarch/identify/identify_files.py
@@ -5,69 +5,70 @@
 # -----------------------------------------------------------------------------
 # Imports
 # -----------------------------------------------------------------------------
-import subprocess
-from pathlib import Path
-from digiarch.data import FileInfo, Identification
-from digiarch.utils.exceptions import IdentificationError
-import yaml
+# import subprocess
+# from pathlib import Path
+# from digiarch.data import FileInfo, Identification
+# from digiarch.utils.exceptions import IdentificationError
+# import yaml
 
 # -----------------------------------------------------------------------------
 # Function Definitions
 # -----------------------------------------------------------------------------
 
 
-def sf_id(file: FileInfo) -> FileInfo:
-    """Function level documentation.
-    Delete non-applicable sections.
+# def sf_id(file: FileInfo) -> FileInfo:
+#     """Function level documentation.
+#     Delete non-applicable sections.
 
-    Parameters
-    ----------
-    input : type
-        description
+#     Parameters
+#     ----------
+#     input : type
+#         description
 
-    Returns
-    -------
-    return : type
-        description
-    type (anonymous types are allowed in return)
-        description
-    Raises
-    ------
-    BadException
-        description
+#     Returns
+#     -------
+#     return : type
+#         description
+#     type (anonymous types are allowed in return)
+#         description
+#     Raises
+#     ------
+#     BadException
+#         description
 
-    """
-    new_id: Identification = Identification(
-        puid=None, mime=None, warning="No identification information obtained."
-    )
-    cmd = subprocess.run(f"sf {file.path}", shell=True, capture_output=True)
+#     """
+#     new_id: Identification = Identification(
+#         puid=None, mime=None, warning=
+# "No identification information obtained."
+#     )
+#     cmd = subprocess.run(f"sf {file.path}", shell=True, capture_output=True)
 
-    try:
-        docs = yaml.safe_load_all(cmd.stdout.decode())
-    except yaml.YAMLError as error:
-        raise IdentificationError(error)
-    else:
-        for doc in docs:
-            if "matches" in doc:
-                for match in doc.get("matches"):
-                    if match.get("ns") == "pronom":
-                        if match.get("id").lower() == "unknown":
-                            new_id = new_id.replace(
-                                warning=match.get("warning").capitalize()
-                            )
-                        else:
-                            new_id = new_id.replace(
-                                puid=match.get("id"),
-                                mime=match.get("mime"),
-                                warning=match.get("warning").capitalize(),
-                            )
-    updated_file: FileInfo = file.replace(identification=new_id)
-    return updated_file
+#     try:
+#         docs = yaml.safe_load_all(cmd.stdout.decode())
+#     except yaml.YAMLError as error:
+#         raise IdentificationError(error)
+#     else:
+#         for doc in docs:
+#             if "matches" in doc:
+#                 for match in doc.get("matches"):
+#                     if match.get("ns") == "pronom":
+#                         if match.get("id").lower() == "unknown":
+#                             new_id = new_id.replace(
+#                                 warning=match.get("warning").capitalize()
+#                             )
+#                         else:
+#                             new_id = new_id.replace(
+#                                 puid=match.get("id"),
+#                                 mime=match.get("mime"),
+#                                 warning=match.get("warning").capitalize(),
+#                             )
+#     updated_file: FileInfo = file.replace(identification=new_id)
+#     return updated_file
 
 
-file = FileInfo(
-    name="test_sheet",
-    ext="",
-    path=Path(r"/home/jnik/Documents/test_data/test_sheet"),
-)
-print(sf_id(file).to_dict().get("identification"))
+# file = FileInfo(
+#     name="test_sheet",
+#     ext="",
+#     path=Path(r"/home/jnik/Documents/test_data/test_sheet"),
+# )
+# print(sf_id(file).to_dict().get("identification"))
