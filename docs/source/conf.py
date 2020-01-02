e704a7f4fbeb1f550a3b815cdbc52e7e0228d6f6
Nina Jensen <jnik@aarhus.dk>
2020-01-02 14:45:37 +0100

    Delete logo file, move README, update theming

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,17 +56,11 @@ add_module_names = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 
-html_theme = "sphinx_typlog_theme"
-html_theme_options = {
-    "color": "#E8371A",
-    "description": "Digital Archive, Aarhus Stadsarkiv",
-    "meta_html": '<meta name="generator" content="sphinx">',
-}
+html_theme = "sphinx_rtd_theme"
 html_theme_path = [".."]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
 
-html_sidebars = {"**": ["logo.html", "globaltoc.html", "searchbox.html"]}
+html_sidebars = {"**": ["globaltoc.html", "searchbox.html"]}
