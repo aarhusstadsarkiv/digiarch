63947cd099dc357688712bb2b8a7cf8c850f2f2c
Nina Jensen <jnik@aarhus.dk>
2020-01-24 09:09:44 +0100

    Add docstring to size_fmt

--- digiarch/data.py
+++ digiarch/data.py
@@ -205,6 +205,18 @@ class DataJSONEncoder(json.JSONEncoder):
 
 
 def size_fmt(size: float) -> str:
+    """Formats a file size in binary multiples to a human readable string.
+
+    Parameters
+    ----------
+    size : float
+        The file size in bytes.
+
+    Returns
+    -------
+    str
+        Human readable string representing size in binary multiples.
+    """
     for unit in ["B", "KiB", "MiB", "GiB", "TiB"]:
         if size < 1024.0:
             break
