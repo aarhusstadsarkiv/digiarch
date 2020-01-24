aa0390e8b2366590d7c14580b2cbc0deb20e323e
Nina Jensen <jnik@aarhus.dk>
2020-01-24 09:07:43 +0100

    Fix conftest fixtures with proper pathlib calls.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,10 @@
 # -----------------------------------------------------------------------------
 # Imports
 # -----------------------------------------------------------------------------
+from datetime import datetime
 import pytest
 from pathlib import Path
+from digiarch.data import FileData, Metadata
 
 # -----------------------------------------------------------------------------
 # Function Definitions
@@ -21,12 +23,20 @@ def temp_dir(tmpdir_factory):
 
 @pytest.fixture
 def main_dir(temp_dir):
-    main_dir: str = temp_dir.mkdir("_digiarch")
+    main_dir: Path = temp_dir / "_digiarch"
+    main_dir.mkdir(exist_ok=True)
     return main_dir
 
 
 @pytest.fixture
 def data_file(main_dir):
-    data_dir: str = main_dir.mkdir(".data")
-    data_file: Path = Path(data_dir, "data.json")
+    data_dir: Path = main_dir / ".data"
+    data_dir.mkdir(exist_ok=True)
+    data_file: Path = data_dir / "data.json"
     return data_file
+
+
+@pytest.fixture
+def file_data(temp_dir):
+    cur_time = datetime.now()
+    return FileData(Metadata(cur_time, Path(temp_dir)))
