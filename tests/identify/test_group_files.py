b449609c4285d2a5ffc3b40396709b44613f3b71
Nina Jensen <jnik@aarhus.dk>
2020-01-24 09:07:25 +0100

    Add ignored file to group files test, no longer xfails

--- tests/identify/test_group_files.py
+++ tests/identify/test_group_files.py
@@ -1,7 +1,7 @@
 import pytest
 from pathlib import Path
 from digiarch.utils.group_files import grouping
-from digiarch.data import FileInfo, to_json
+from digiarch.data import FileInfo
 
 
 @pytest.fixture
@@ -20,15 +20,23 @@ def test_file_1(temp_dir):
     return test_file
 
 
+@pytest.fixture
+def ignored_file(temp_dir):
+    test_file = Path(temp_dir).joinpath("test2.exe")
+    with test_file.open("w") as file:
+        file.write("1")
+    return test_file
+
+
 class TestGrouping:
-    @pytest.mark.xfail
-    def test_with_file(self, main_dir, data_file, test_file_0, test_file_1):
+    def test_with_file(self, main_dir, test_file_0, test_file_1, ignored_file):
         file_1 = FileInfo(path=test_file_0)
         file_2 = FileInfo(path=test_file_1)
-        file_info = [file_1, file_2]
-        to_json(file_info, data_file)
+        file_3 = FileInfo(path=ignored_file)
+        file_info = [file_1, file_2, file_3]
 
-        grouping(data_file, main_dir)
+        grouping(file_info, main_dir)
         contents = [str(p) for p in Path(main_dir).iterdir()]
         assert any("txt_files.txt" in content for content in contents)
         assert any("pdf_files.txt" in content for content in contents)
+        assert any("ignored_files.txt" in content for content in contents)
