881ff5bf8e484bd474ff5ec20e5828e69b83f24a
Nina Jensen <jnik@aarhus.dk>
2020-03-02 17:16:49 +0100

    Update checksum tests to reflect use of BLAKE2

--- tests/identify/test_checksums.py
+++ tests/identify/test_checksums.py
@@ -36,12 +36,9 @@ class TestFileChecksum:
     def test_without_file(self, temp_dir):
         assert file_checksum(Path(temp_dir)) == ""
 
-    def test_insecure(self, test_file_0):
-        assert file_checksum(test_file_0) == "ab76e8e9ff682bb4"
-
-    def test_secure(self, test_file_0):
+    def test_with_file(self, test_file_0):
         assert (
-            file_checksum(test_file_0, secure=True)
+            file_checksum(test_file_0)
             == "e9f11462495399c0b8d0d8ec7128df9c0d7269cda23531a352b174bd29c3"
             "b6318a55d3508cb70dad9aaa590185ba0fef4fab46febd46874a103739c10d6"
             "0ebc7"
@@ -54,12 +51,9 @@ class TestChecksumWorker:
         result = checksum_worker(file_info)
         assert result.checksum is None
 
-    def test_insecure(self, test_file_info):
-        assert checksum_worker(test_file_info).checksum == "ab76e8e9ff682bb4"
-
-    def test_secure(self, test_file_info):
+    def test_with_file(self, test_file_info):
         assert (
-            checksum_worker(test_file_info, secure=True).checksum
+            checksum_worker(test_file_info).checksum
             == "e9f11462495399c0b8d0d8ec7128df9c0d7269cda23531a352b174bd29c3"
             "b6318a55d3508cb70dad9aaa590185ba0fef4fab46febd46874a103739c10d6"
             "0ebc7"
@@ -105,12 +99,13 @@ class TestCheckDuplicates:
         f_info_1 = FileInfo(path=test_file_0, checksum="test1")
         files = [f_info_0, f_info_1]
         updated_files = generate_checksums(files)
+        print(updated_files)
         check_duplicates(updated_files, temp_dir)
         outfile = Path(temp_dir).joinpath("duplicate_files.json")
         with outfile.open() as file:
             result = json.load(file)
-        assert file_checksum(test_file_0, secure=True) in result
-        assert len(result[file_checksum(test_file_0, secure=True)]) == 2
+        assert file_checksum(test_file_0) in result
+        assert len(result[file_checksum(test_file_0)]) == 2
 
     def test_without_dups(self, test_file_0, test_file_1, temp_dir):
         f_info_0 = FileInfo(path=test_file_0, checksum="test0")
@@ -121,6 +116,6 @@ class TestCheckDuplicates:
         outfile = Path(temp_dir).joinpath("duplicate_files.json")
         with outfile.open() as file:
             result = json.load(file)
-        assert file_checksum(test_file_0, secure=True) not in result
-        assert file_checksum(test_file_1, secure=True) not in result
+        assert file_checksum(test_file_0) not in result
+        assert file_checksum(test_file_1) not in result
         assert result == {}
