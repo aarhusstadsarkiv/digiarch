05b13d6d775ccadaeb03c91cb9f82f8a190e6fc2
Nina Jensen <jnik@aarhus.dk>
2020-03-04 14:57:45 +0100

    Add warning count per grouping

--- digiarch/identify/reports.py
+++ digiarch/identify/reports.py
@@ -31,6 +31,7 @@ def report_results(files: List[FileInfo], save_path: Path) -> None:
     ext_count: Counter = Counter()
     id_warnings: Dict[str, List[Dict[str, Identification]]] = dict()
     warning_key: str
+    warning_count: Counter = Counter()
 
     # Collect information
     for file in files:
@@ -43,8 +44,8 @@ def report_results(files: List[FileInfo], save_path: Path) -> None:
             warning_list = id_warnings.get(warning_key, [])
             warning_list.append({str(file.path): file.identification})
             id_warnings.update({warning_key: warning_list})
-    file_exts: Dict[str, int] = dict(ext_count.most_common())
 
+    file_exts: Dict[str, int] = dict(ext_count.most_common())
     if files:
         # Create new folder in save path
         save_path = save_path / "reports"
@@ -53,4 +54,10 @@ def report_results(files: List[FileInfo], save_path: Path) -> None:
         # Save files
         to_json(file_exts, save_path / "file_extensions.json")
         if id_warnings:
-            to_json(id_warnings, save_path / "identification_warnings.json")
+            for warning_key, warning_list in id_warnings.items():
+                warning_count.update({warning_key: len(warning_list)})
+
+            to_json(
+                [dict(warning_count), id_warnings],
+                save_path / "identification_warnings.json",
+            )
