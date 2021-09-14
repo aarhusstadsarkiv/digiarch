6b895f1349cdcd6e3670e5b526c6a9aef40428e7
Aryan Mohammadi Landi <arml@aarhus.dk>
2021-09-14 15:53:58 +0200

    Refactor of custom_id.

--- digiarch/core/identify_files.py
+++ digiarch/core/identify_files.py
@@ -24,26 +24,17 @@ import json
 # Function Definitions
 # -----------------------------------------------------------------------------
 
-
-def apply_map(bof_map, eof_map, bof, eof, operator):
-    if operator == "OR":
-        return bof_map.match(bof) or eof_map.search(eof)
-    elif operator == "AND":
-        return bof_map.match(bof) and eof_map.search(eof)
+def update_file_id(path, file_id, signature):
+    file_id.puid = signature["puid"]
+    file_id.signature = signature["signature"]
+    if path.suffix.lower() != signature["extension"]:
+        file_id.warning = "Extension mismatch"
     else:
-        raise ValueError("operator must be OR or AND")
+        file_id.warning = None
 
 def custom_id(path: Path, file_id: Identification) -> Identification:
     sig_file = Path(__file__).parent / "custom_sigs.json"
     signatures: List[Dict] = json.load(sig_file.open(encoding="utf-8"))
-    maps = {}
-    for signature in signatures:
-        maps[signature["extension"]] = signature
-    sig_mmap = re.compile(maps[".mmap"]["bof"])
-    sig_gif_bof = re.compile(maps[".gif"])["bof"]
-    sig_gif_eof = re.compile(maps[".gif"])["eof"]
-    sig_nsf_bof = re.compile(maps[".nsf"]["bof"])
-    sig_nsf_eof = re.compile(maps[".nsf"]["eof"])
     
     with path.open("rb") as file_bytes:
         # BOF
@@ -55,7 +46,7 @@ def custom_id(path: Path, file_id: Identification) -> Identification:
             # File too small :)
             file_bytes.seek(-file_bytes.tell(), 2)
         eof = file_bytes.read(1024).hex()
-    if apply_map(sig_mmap, sig_mmap, bof, eof, "OR"):
+    '''if apply_map(sig_mmap, sig_mmap, bof, eof, "OR"):
         file_id.puid = "aca-fmt/4"
         file_id.signature = "MindManager Mind Map"
         if path.suffix.lower() != ".mmap":
@@ -79,19 +70,25 @@ def custom_id(path: Path, file_id: Identification) -> Identification:
         if path.suffix.lower() != ".nsf":
             file_id.warning = "Extension mismatch"
         else:
-            file_id.warning = None
-    else:
-        for sig in signatures:
+            file_id.warning = None'''
+    
+    for sig in signatures:
+        if "bof" in sig:
+            bof_pattern = re.compile(sig["bof"])
+            eof_pattern = re.compile(sig["eof"])
+            if sig["operator"] == "OR":
+                if bof_pattern.search(bof) or eof_pattern.search(eof):
+                    update_file_id(path, file_id, sig)
+                    break
+            elif sig["operator"] == "AND":
+                if bof_pattern.search(bof) and eof_pattern.search(eof):
+                    update_file_id(path, file_id, sig)
+                    break
+        else:
             pattern = re.compile(sig["pattern"])
             if pattern.search(bof):
-                file_id.puid = sig["puid"]
-                file_id.signature = sig["signature"]
-                if path.suffix.lower() != sig["extension"]:
-                    file_id.warning = "Extension mismatch"
-                else:
-                    file_id.warning = None
+                update_file_id(path, file_id, sig)
                 break
-
     return file_id
 
 
