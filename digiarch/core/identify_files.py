e1e50dbc9a799cfb7b2b0e5db53242b67b92ee41
Aryan Mohammadi Landi <arml@aarhus.dk>
2022-02-24 13:35:53 +0100

    Added unit name to db.

--- digiarch/core/identify_files.py
+++ digiarch/core/identify_files.py
@@ -241,6 +241,7 @@ def update_file_info(
         )
     file_info = file_info.copy(update=new_id.dict())
     file_info.is_binary = is_binary(file_info)
+    file_info.file_size = file_path.stat().st_size
     return file_info
 
 
