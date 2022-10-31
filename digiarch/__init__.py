5b9b759bdedcd73b42b71f9ccb743db3f77a8c3e
Claus Juhl Knudsen <cjk@aarhus.dk>
2022-10-31 19:36:56 +0100

    Fix broken version info

--- digiarch/__init__.py
+++ digiarch/__init__.py
@@ -1,5 +1,17 @@
 # This is how to get version information
 # NB! Doesn't work with pyinstaller
 # from importlib.metadata import version  # type: ignore
+from pathlib import Path
 
-__version__ = "0.9.18"  # version("digiarch")
+
+def get_version() -> str:
+    version: str = "Ukendt version"
+    with open(Path(__file__).absolute().parent.parent / "pyproject.toml") as i:
+        for line in i.readlines():
+            if line.startswith("version"):
+                version = line[line.index('"') + 1 : -2]
+    return version
+
+
+# __version__ = "0.9.18"
+__version__ = get_version()
