e91283c7ee293a9ea199dc7915de9ba2da1995d6
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-08-19 10:27:28 +0200

    upgrade:command_upgrade - do not use start_program before calling upgrade

--- digiarch/upgrade.py
+++ digiarch/upgrade.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from logging import INFO
 from pathlib import Path
 from shutil import copy2
 
@@ -36,15 +38,17 @@ def command_upgrade(ctx: Context, root: Path, backup: bool):
     When using --backup, a copy of the current database version will be created in the same folder with the name
     "files-{version}.db". The copy will not be created if the database is already at the latest version.
     """
+    start_time: datetime = datetime.now()
+
     with FileDB(root / "_metadata" / "files.db", check_version=False) as database:
-        log_file, log_stdout = start_program(ctx, database, None, True, True)
         updated: bool = False
+        log_file = log_stdout = None
 
         with ExceptionManager(BaseException, allow=[ClickException]) as exception:
             if not is_latest(database):
                 if backup:
                     backup_path = database.path.with_stem(database.path.stem + f"-{database.metadata.select().version}")
-                    if backup_path.exists():
+                    if backup_path.exists() and database.path.stat().st_size != backup_path.stat().st_size:
                         raise BadParameter(
                             f"Backup file {backup_path.name} already exists.",
                             ctx,
@@ -59,7 +63,14 @@ def command_upgrade(ctx: Context, root: Path, backup: bool):
                 )
                 database.upgrade()
                 database.init()
+                log_file, log_stdout = start_program(ctx, database, start_time, True, True)
                 database.history.insert(event)
+                event.log(INFO, log_stdout)
                 updated = True
+            else:
+                log_file, log_stdout = start_program(ctx, database, start_time, False, True, True)
+                HistoryEntry.command_history(ctx, "skip", reason="Database is already at the latest version").log(
+                    INFO, log_stdout
+                )
 
         end_program(ctx, database, exception, updated, log_file, log_stdout)
