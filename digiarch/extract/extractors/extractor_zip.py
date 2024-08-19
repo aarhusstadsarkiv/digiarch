b05be000c5b95b4e3daa5826836178227783b7f6
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-08-19 14:11:59 +0200

    extract.extractors.extractor_zip:ZipExtractor - handle BadZipFile and LargeZipFile exceptions

--- digiarch/extract/extractors/extractor_zip.py
+++ digiarch/extract/extractors/extractor_zip.py
@@ -1,8 +1,11 @@
 from pathlib import Path
 from typing import ClassVar
 from typing import Generator
+from zipfile import BadZipFile
+from zipfile import LargeZipFile
 from zipfile import ZipFile
 
+from .base import ExtractError
 from .base import ExtractorBase
 from .base import PasswordProtectedError
 
@@ -13,11 +16,14 @@ class ZipExtractor(ExtractorBase):
     def extract(self) -> Generator[Path, None, None]:
         extract_folder: Path = self.extract_folder
 
-        with ZipFile(self.file.get_absolute_path()) as zf:
-            for member in zf.infolist():
-                if member.is_dir():
-                    continue
-                elif member.flag_bits & 0b1:
-                    raise PasswordProtectedError(self.file)
-                extract_folder.joinpath(member.filename).parent.mkdir(parents=True, exist_ok=True)
-                yield Path(zf.extract(member, extract_folder))
+        try:
+            with ZipFile(self.file.get_absolute_path()) as zf:
+                for member in zf.infolist():
+                    if member.is_dir():
+                        continue
+                    elif member.flag_bits & 0b1:
+                        raise PasswordProtectedError(self.file)
+                    extract_folder.joinpath(member.filename).parent.mkdir(parents=True, exist_ok=True)
+                    yield Path(zf.extract(member, extract_folder))
+        except (BadZipFile, LargeZipFile) as e:
+            raise ExtractError(self.file, repr(e))
