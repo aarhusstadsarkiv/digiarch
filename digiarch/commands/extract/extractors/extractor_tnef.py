db5fdf5de1c73e76592691e01a4b02ecc71265bd
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-10-03 11:46:14 +0200

    extractors.extractor_tnef - remove body extraction
    TNEF files are converted with convertool

--- digiarch/commands/extract/extractors/extractor_tnef.py
+++ digiarch/commands/extract/extractors/extractor_tnef.py
@@ -29,20 +29,3 @@ class TNEFExtractor(ExtractorBase):
             with path.open("wb") as oh:
                 oh.write(attachment.data)
                 yield path, attachments_folder.joinpath(name)
-
-        if tnef.htmlbody:
-            body_path: Path = extract_folder.joinpath(f"{self.file.relative_path.stem}_body.html")
-            body_path.write_text(tnef.htmlbody)
-            yield body_path, body_path
-        elif tnef.rtfbody:
-            body_path: Path = extract_folder.joinpath(f"{self.file.relative_path.stem}_body.rtf")
-            body_path.write_bytes(tnef.rtfbody)
-            yield body_path, body_path
-        elif tnef.body and isinstance(tnef.body, bytes):
-            body_path: Path = extract_folder.joinpath(f"{self.file.relative_path.stem}_body.txt")
-            body_path.write_bytes(tnef.body)
-            yield body_path, body_path
-        elif tnef.body:
-            body_path: Path = extract_folder.joinpath(f"{self.file.relative_path.stem}_body.txt")
-            body_path.write_text(tnef.body)
-            yield body_path, body_path
