8bd09b3da764ef282567144243bf8989e77a25c3
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-11-12 10:25:02 +0100

    common:AVID - add classes to handle AVID directories and database path

--- digiarch/common.py
+++ digiarch/common.py
@@ -1,4 +1,6 @@
+from os import PathLike
 from pathlib import Path
+from re import match
 
 import yaml
 from acacore.models.reference_files import Action
@@ -13,6 +15,174 @@ from click import option
 from click import Parameter
 from click import Path as ClickPath
 from pydantic import TypeAdapter
+
+
+# noinspection PyPep8Naming
+class AVIDIndices:
+    def __init__(self, avid_dir: Path):
+        self.avid_dir = avid_dir
+
+    @property
+    def path(self):
+        return self.avid_dir / "Indices"
+
+    @property
+    def archiveIndex(self) -> Path:
+        """Indices/archiveIndex.xml"""
+        return self.path / "archiveIndex.xml"
+
+    @property
+    def contextDocumentationIndex(self) -> Path:
+        """Indices/contextDocumentationIndex.xml"""
+        return self.path / "contextDocumentationIndex.xml"
+
+    @property
+    def docIndex(self) -> Path:
+        """Indices/docIndex.xml"""
+        return self.path / "docIndex.xml"
+
+    @property
+    def fileIndex(self) -> Path:
+        """Indices/fileIndex.xml"""
+        return self.path / "fileIndex.xml"
+
+    @property
+    def tableIndex(self) -> Path:
+        """Indices/tableIndex.xml"""
+        return self.path / "tableIndex.xml"
+
+
+# noinspection PyPep8Naming
+class AVIDSchemas:
+    def __init__(self, avid_dir: Path):
+        self.avid_dir: Path = avid_dir
+
+    @property
+    def path(self):
+        return self.avid_dir / "Schemas"
+
+    @property
+    def archiveIndex(self) -> Path:
+        """Schemas/standard/archiveIndex.xsd"""
+        return self.path / "standard" / "archiveIndex.xsd"
+
+    @property
+    def contextDocumentationIndex(self) -> Path:
+        """Schemas/standard/contextDocumentationIndex.xsd"""
+        return self.path / "standard" / "contextDocumentationIndex.xsd"
+
+    @property
+    def docIndex(self) -> Path:
+        """Schemas/standard/docIndex.xsd"""
+        return self.path / "standard" / "docIndex.xsd"
+
+    @property
+    def fileIndex(self) -> Path:
+        """Schemas/standard/fileIndex.xsd"""
+        return self.path / "standard" / "fileIndex.xsd"
+
+    @property
+    def researchIndex(self) -> Path:
+        """Schemas/standard/researchIndex.xsd"""
+        return self.path / "standard" / "researchIndex.xsd"
+
+    @property
+    def tableIndex(self) -> Path:
+        """Schemas/standard/tableIndex.xsd"""
+        return self.path / "standard" / "tableIndex.xsd"
+
+    @property
+    def XMLSchema(self) -> Path:
+        """Schemas/standard/XMLSchema.xsd"""
+        return self.path / "standard" / "XMLSchema.xsd"
+
+    @property
+    def tables(self) -> dict[int, Path]:
+        """Tables/tableN/tableN.xsd"""
+        return {
+            int(f.name.removeprefix("table")): f.joinpath(f.name).with_suffix(".xsd")
+            for f in self.avid_dir.joinpath("Tables").iterdir()
+            if f.is_dir() and match(r"table\d+", f.name)
+        }
+
+
+class AVIDDirs:
+    def __init__(self, avid_dir: Path):
+        self.dir: Path = avid_dir
+
+    @property
+    def original_documents(self):
+        return self.dir / "OriginalDocuments"
+
+    @property
+    def master_documents(self):
+        return self.dir / "MasterDocuments"
+
+    @property
+    def access_documents(self):
+        return self.dir / "AccessDocuments"
+
+    @property
+    def documents(self):
+        return self.dir / "Documents"
+
+    @property
+    def indices(self) -> AVIDIndices:
+        """Indices"""
+        return AVIDIndices(self.dir)
+
+    @property
+    def schemas(self) -> AVIDSchemas:
+        """Schemas"""
+        return AVIDSchemas(self.dir)
+
+    @property
+    def tables(self) -> dict[int, Path]:
+        """Tables"""
+        return {
+            int(f.name.removeprefix("table")): f.joinpath(f.name).with_suffix(".xml")
+            for f in self.dir.joinpath("Tables").iterdir()
+            if f.is_dir() and match(r"table\d+", f.name)
+        }
+
+
+class AVID:
+    def __init__(self, directory: str | PathLike):
+        if not self.is_avid_dir(directory):
+            raise ValueError(f"{directory} is not a valid AVID directory")
+
+        self.path: Path = Path(directory).resolve()
+        self.dirs: AVIDDirs = AVIDDirs(self.path)
+
+    @classmethod
+    def is_avid_dir(cls, directory: str | PathLike[str]) -> bool:
+        directory = Path(directory)
+        if not directory.is_dir():
+            return False
+        if not (avid_dirs := AVIDDirs(directory)).indices.path.is_dir():
+            return False
+        if not avid_dirs.schemas.path.is_dir():
+            return False
+        if not avid_dirs.original_documents.is_dir():
+            return False
+        return True
+
+    @classmethod
+    def find_database_root(cls, directory: str | PathLike[str]) -> Path | None:
+        directory = Path(directory)
+        if directory.joinpath("_metadata", "avid.db").is_file():
+            return directory
+        if directory.parent != directory:
+            return cls.find_database_root(directory.parent)
+        return None
+
+    @property
+    def metadata_dir(self):
+        return self.path / "_metadata"
+
+    @property
+    def database_path(self):
+        return self.metadata_dir / "avid.db"
 
 
 def argument_root(exists: bool):
