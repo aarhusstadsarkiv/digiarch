83e309f47f1266c557b06dada4a31ce0d9c549af
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-11-12 10:28:40 +0100

    commands.upgrade - convert to use new AVID and database objects

--- digiarch/commands/upgrade.py
+++ digiarch/commands/upgrade.py
@@ -1,11 +1,10 @@
 from logging import INFO
-from pathlib import Path
 from shutil import copy2
 
 from acacore.__version__ import __version__ as __acacore_version__
-from acacore.database import FileDB
+from acacore.database import FilesDB
 from acacore.database.upgrade import is_latest
-from acacore.models.history import HistoryEntry
+from acacore.models.event import Event
 from acacore.utils.click import end_program
 from acacore.utils.click import start_program
 from acacore.utils.helpers import ExceptionManager
@@ -17,12 +16,13 @@ from click import option
 from click import pass_context
 
 from digiarch.__version__ import __version__
-from digiarch.common import argument_root
+from digiarch.common import AVID
 from digiarch.common import ctx_params
+from digiarch.common import option_avid
 
 
 @command("upgrade", no_args_is_help=True, short_help="Upgrade the database.")
-@argument_root(True)
+@option_avid()
 @option(
     "--backup/--no-backup",
     is_flag=True,
@@ -31,21 +31,21 @@ from digiarch.common import ctx_params
     help="Backup current version.",
 )
 @pass_context
-def command_upgrade(ctx: Context, root: Path, backup: bool):
+def cmd_upgrade(ctx: Context, avid: AVID, backup: bool):
     """
     Upgrade the files' database to the latest version of acacore.
 
     When using --backup, a copy of the current database version will be created in the same folder with the name
-    "files-{version}.db". The copy will not be created if the database is already at the latest version.
+    "avid-{version}.db". The copy will not be created if the database is already at the latest version.
     """
-    with FileDB(root / "_metadata" / "files.db", check_version=False) as database:
+    with FilesDB(avid.database_path, check_initialisation=True, check_version=False) as database:
         log_file, log_stdout, start_event = start_program(ctx, database, __version__, None, True, True, True)
         updated: bool = False
 
         with ExceptionManager(BaseException, allow=[ClickException]) as exception:
-            if not is_latest(database):
+            if not is_latest(database.connection):
                 if backup:
-                    backup_path = database.path.with_stem(database.path.stem + f"-{database.metadata.select().version}")
+                    backup_path = database.path.with_stem(database.path.stem + f"-{database.metadata.get().version}")
                     if backup_path.exists() and database.path.stat().st_size != backup_path.stat().st_size:
                         raise BadParameter(
                             f"Backup file {backup_path.name} already exists.",
@@ -53,21 +53,23 @@ def command_upgrade(ctx: Context, root: Path, backup: bool):
                             ctx_params(ctx)["backup"],
                         )
                     copy2(database.path, backup_path)
-                event = HistoryEntry.command_history(
+                event = Event.command_history(
                     ctx,
                     "update",
                     None,
-                    [database.metadata.select().version, __acacore_version__],
+                    [database.metadata.get().version, __acacore_version__],
                 )
                 database.upgrade()
                 database.init()
-                database.history.insert(start_event)
-                database.history.insert(event)
+                database.log.insert(start_event)
+                database.log.insert(event)
                 event.log(INFO, log_stdout)
                 updated = True
             else:
-                HistoryEntry.command_history(ctx, "skip", reason="Database is already at the latest version").log(
-                    INFO, log_stdout
-                )
+                Event.command_history(
+                    ctx,
+                    "skip",
+                    reason="Database is already at the latest version",
+                ).log(INFO, log_stdout)
 
         end_program(ctx, database, exception, not updated, log_file, log_stdout)
