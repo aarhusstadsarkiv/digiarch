9bf3b5d4d6792c17dc5e38f4b40c1fff99b49560
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-11-12 10:23:19 +0100

    commands.completions:command_completions - rename to cmd_completions

--- digiarch/commands/completions.py
+++ digiarch/commands/completions.py
@@ -41,7 +41,7 @@ class ShellChoice(CompleteChoice):
 @argument("shell", type=ShellChoice(), callback=lambda _c, _p, v: get_completion_class(v))
 @pass_context
 @docstring_format(shells="\n    ".join(f"* {s.value}\t{s.help}" for s in ShellChoice.completion_items))
-def command_completions(ctx: Context, shell: Type[ShellComplete] | None):
+def cmd_completions(ctx: Context, shell: Type[ShellComplete] | None):
     """
     Generate tab-completion scripts for your shell.
 
