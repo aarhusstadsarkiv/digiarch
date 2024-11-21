4177fd4ac292c63c7385cf31adcd2505d00f95b1
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-11-21 10:34:05 +0100

    workflows.get_changelog - format

--- .github/workflows/get_changelog.py
+++ .github/workflows/get_changelog.py
@@ -5,15 +5,15 @@ def main(file: str, tag: str) -> str:
     header: str = f"## v{tag.lstrip('v')}"
     body: list[str] = []
     is_body: bool = False
-    with open(file, "r", encoding="utf-8") as f:
+    with open(file, encoding="utf-8") as f:
         while line := f.readline():
             line = line.rstrip()
             if is_body and line.startswith("## "):
                 break
-            elif line == header or line.startswith(f"{header} "):
+            if line == header or line.startswith(f"{header} "):
                 is_body = True
                 continue
-            elif is_body and line.startswith("#"):
+            if is_body and line.startswith("#"):
                 body.append(line[1:])
             elif is_body:
                 body.append(line)
