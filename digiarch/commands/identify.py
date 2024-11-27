6939c93a183bd17ee8603d273a7e7f75e216890b
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-11-27 13:18:38 +0100

    commands.identify:grp_identify - avoid sorting of commands

--- digiarch/commands/identify.py
+++ digiarch/commands/identify.py
@@ -483,3 +483,6 @@ def cmd_identify_master(
                     )
 
         end_program(ctx, db, exception, dry_run, log_file, log_stdout)
+
+
+grp_identify.list_commands = lambda _ctx: list(grp_identify.commands)
