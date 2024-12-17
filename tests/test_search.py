8c45f6e17f3143e2ea828d4ca14836916de56b7a
Matteo Campinoti <matteo.campinoti94@gmail.com>
2024-12-17 14:00:06 +0100

    tests.log,.search - add tests for log and search commands

---
+++ tests/test_search.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from digiarch.cli import app
+from digiarch.common import AVID
+from tests.conftest import run_click
+
+
+def test_search(avid_folder_copy: Path):
+    avid = AVID(avid_folder_copy)
+
+    run_click(avid.path, app, "search", "original", "@relative_path %")
+    run_click(avid.path, app, "search", "master", "@relative_path %")
+    run_click(avid.path, app, "search", "access", "@relative_path %")
+    run_click(avid.path, app, "search", "statutory", "@relative_path %")
