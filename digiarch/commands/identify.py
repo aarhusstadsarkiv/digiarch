52fb5804a3cced9a14f40cb05960965d8cffe641
Matteo Campinoti <matteo.campinoti94@gmail.com>
2025-01-16 14:34:44 +0100

    commands.identify:identify_original_file - add output when a file is skipped because it is locked

--- digiarch/commands/identify.py
+++ digiarch/commands/identify.py
@@ -157,6 +157,12 @@ def identify_original_file(
     if existing_file and not update:
         return
     if existing_file and existing_file.lock and not ignore_lock:
+        Event.from_command(ctx, "skip", (existing_file.uuid, "original")).log(
+            INFO,
+            *loggers,
+            locked=True,
+            path=existing_file.relative_path,
+        )
         return
 
     file = OriginalFile.from_file(siegfried_file.filename, avid.path, parent=parent)
