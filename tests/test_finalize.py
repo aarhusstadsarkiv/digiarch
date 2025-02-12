6173b3ba07ee40851629b4d1f0d84d341e3d084e
Matteo Campinoti <matteo.campinoti94@gmail.com>
2025-02-12 13:10:44 +0100

    tests.finalize:doc_index - add test for `finalize doc-index` command

--- tests/test_finalize.py
+++ tests/test_finalize.py
@@ -1,8 +1,10 @@
 from math import ceil
 from pathlib import Path
 
+import pytest
 from acacore.database import FilesDB
 from acacore.models.file import ConvertedFile
+from click import BadParameter
 
 from digiarch.cli import app
 from digiarch.common import AVID
@@ -33,3 +35,10 @@ def test_finalize_docs_collections(avid_folder_copy: Path):
             test_file: ConvertedFile | None = database.statutory_files[{"uuid": str(file.uuid)}]
             assert test_file
             assert test_file.relative_path == expected_paths[file.relative_path]
+
+
+def test_finalize_doc_index(avid_folder_copy: Path):
+    run_click(avid_folder_copy, app, "finalize", "doc-index", "--media-id", "AVID.SA.19001.1")
+
+    with pytest.raises(BadParameter, match=f"{'invalid'!r} not in format"):
+        run_click(avid_folder_copy, app, "finalize", "doc-index", "--media-id", "invalid")
